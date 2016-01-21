eda143afe9727160faf4967ef2a1e13e38ff4ef6
Jeremiah Svaren <Jeremiah.svaren@gmail.com>
2016-01-20 23:36:35 -0600

    Day 3 and a bit of file restructuring
    project number 3 done and some better organization of files.
    Also made the 1st page look a little bit better.

---
+++ day_3/scripts/race.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+$(".frog").hover(function(){
+    animateDiv();
+    
+});
+
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 50;
+    var w = $(window).width() - 50;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
+
+function animateDiv(){
+    var newq = makeNewPosition();
+    $('.frog').animate({ top: newq[0], left: newq[1] }, function(){
+              
+    });
+    
+};
